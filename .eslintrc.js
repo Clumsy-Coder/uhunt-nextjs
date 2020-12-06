394c700f8e2a04e433cb8a341520922962eff0af
Clumsy-Coder <ubari94@gmail.com>
2020-12-06 10:10:45 -0700

    chore(eslint): add .eslintrc.js config file

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'airbnb-typescript',
+    'prettier',
+    'prettier/babel',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    // 'plugin:redux-saga/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:jest/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    '@typescript-eslint',
+    'eslint-plugin-tsdoc',
+    'react',
+    // 'redux-saga',
+    'prettier',
+    'jest',
+    'jsx-a11y',
+  ],
+  rules: {
+    'react/jsx-one-expression-per-line': ['off'],
+    'max-len': [
+      'error',
+      {
+        code: 100,
+        tabWidth: 2,
+        ignoreComments: true,
+        ignoreUrls: true,
+      },
+    ],
+    'react/jsx-props-no-spreading': ['off'],
+    'tsdoc/syntax': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
+  ignorePatterns: ['test-results', 'metro.config.js', 'coverage', '.eslintrc.js'],
+};
